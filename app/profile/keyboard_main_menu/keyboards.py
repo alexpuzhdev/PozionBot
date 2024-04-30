436068c34607c0fc627d55bd7f0e11cfd75c2a2f
alexpuzhdev <168548067+alexpuzhdev@users.noreply.github.com>
2024-04-30 15:41:32 +0300

    вернул блок кода с клавиатурой для проверки внесенных изменений

--- app/profile/keyboard_main_menu/keyboards.py
+++ app/profile/keyboard_main_menu/keyboards.py
@@ -10,3 +10,14 @@ main_kb = [
      KeyboardButton(text='FAQ')],
 ]
 
+main = ReplyKeyboardMarkup(keyboard=main_kb,
+                           resize_keyboard=True)
+
+profile = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text='Мои покупки', callback_data='my_purchase'),
+     InlineKeyboardButton(text='Статус доставки', callback_data='delivery_status')],
+    [InlineKeyboardButton(text='Корзина', callback_data='my_cart'),
+     InlineKeyboardButton(text='Избранное', callback_data='my_favorites')],
+    [InlineKeyboardButton(text='Редактировать данные', callback_data='edit_info')],
+    [InlineKeyboardButton(text='🔙 Главное меню', callback_data='main_menu')]
+])
