3e92963227304428854cecf870741abb4d902634
alexpuzhdev <168548067+alexpuzhdev@users.noreply.github.com>
2024-05-01 20:16:21 +0300

    отредактировал клавиатуру пользователя

--- app/profile/user/keyboards/ProfileKeyboards.py
+++ app/profile/user/keyboards/ProfileKeyboards.py
@@ -8,7 +8,7 @@ from aiogram.types import WebAppInfo
 profile_user = InlineKeyboardMarkup(inline_keyboard=[
         [InlineKeyboardButton(text='Активные заказы', callback_data='delivery_status'),
          InlineKeyboardButton(text='Завершенные', callback_data='my_cart')],
-        [InlineKeyboardButton(text='Избранное', callback_data='my_favorites')],
         [InlineKeyboardButton(text='Редактировать данные', callback_data='edit_info')],
+        [InlineKeyboardButton(text='Обратиться в поддержку', callback_data='get_help')],
         [InlineKeyboardButton(text='🔙 Главное меню', callback_data='main_menu')]
 ])
